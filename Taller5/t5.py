5158c5519d49283d47700f408b68ab7a515413b4
earosselot <earosselot@gmail.com>
2019-10-29 10:15:39 -0300

    Taller5 limpio

---
+++ Taller5/t5.py
@@ -0,0 +1,32 @@
+import numpy as np
+import time
+import random
+import sys
+
+global N
+N = 1000
+
+def matrizCeros():
+    # COMPLETAR
+    pass
+
+def matrizAleatoria(a):
+    # COMPLETAR
+    pass
+
+def multMatrices(a, b, res):
+    # COMPLETAR
+    pass
+
+def medirTiempos(fn, *args):
+    # COMPLETAR
+    pass
+
+def realizarExperimento():
+    # COMPLETAR
+    pass
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        N = int(sys.argv[1])
+    realizarExperimento()
